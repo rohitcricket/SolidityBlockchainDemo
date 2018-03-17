c043538144616564b066b3d2079a058115785509
Rohit Gupta <rohitcricket@gmail.com>
2018-03-17 11:25:25 -0700

    first commit

---
+++ test/codertoken.js
@@ -0,0 +1,59 @@
+var CoderToken = artifacts.require("./CoderToken.sol");
+
+contract("CoderToken", function(accounts) {
+	it("Should put 15000 CoderToken in the first account", function() {
+		return CoderToken.deployed()
+			.then(function(instance) {
+				return instance.balanceOf.call(accounts[0]);
+			})
+			.then(function(balance) {
+				assert.equal(balance.valueOf(), 15000, "15,000 CoderToken wasn't in the first account");
+			});
+	});
+	it("Should send 10 CoderToken correctly", function() {
+		var token;
+		var account_one = accounts[0];
+		var account_two = accounts[1];
+
+		var account_one_starting_balance;
+		var account_two_starting_balance;
+		var account_one_ending_balance;
+		var account_two_ending_balance;
+
+		var amount = 10;
+
+		return CoderToken.deployed()
+			.then(function(instance) {
+				token = instance;
+				return token.balanceOf.call(account_one);
+			})
+			.then(function(balance) {
+				account_one_starting_balance = balance.toNumber();
+				return token.balanceOf.call(account_two);
+			})
+			.then(function(balance) {
+				account_two_starting_balance = balance.toNumber();
+				return token.transfer(account_two, amount, { from: account_one });
+			})
+			.then(function() {
+				return token.balanceOf.call(account_one);
+			})
+			.then(function(balance) {
+				account_one_ending_balance = balance.toNumber();
+				return token.balanceOf.call(account_two);
+			})
+			.then(function(balance) {
+				account_two_ending_balance = balance.toNumber();
+				assert.equal(
+					account_one_ending_balance,
+					account_one_starting_balance - amount,
+					"Amount was not successfully transferred from sender."
+				);
+				assert.equal(
+					account_two_ending_balance,
+					account_two_starting_balance + amount,
+					"Amount was not successfully sent to receiver."
+				);
+			});
+	});
+});
